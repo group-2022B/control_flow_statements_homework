17268c04a7af74ec33022c895a8663ae8d900d85
yoshlikmedia <yoshlikmedia@gmail.com>
2022-03-23 17:10:30 +0500

    answer

--- if09.py
+++ if09.py
@@ -9,10 +9,5 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    if a < 10:
-        return True
-    else:
-        a = str(a)
-        a = a[1] + a[0]
-        a = int(a)
-        return main(a)
+    return a >= int(str(a)[::-1])
+
